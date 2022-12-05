5ee23014584b47bc95243985f675a7dbb09e5d0a
mE <metaescape@foxmail.com>
2022-12-05 23:08:28 +0800

    support filename with spaces

--- pptsnap.py
+++ pptsnap.py
@@ -6,25 +6,30 @@ import subprocess
 import hashlib
 import shutil
 
+
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--link', type=str, default='')
-    parser.add_argument('-p', '--page', type=str, default='0')
-    parser.add_argument('-d', '--directory', type=str, default='/tmp')
-    parser.add_argument('-s', '--scale', type=int, default=0)
+    parser.add_argument("-l", "--link", type=str, default="")
+    parser.add_argument("-p", "--page", type=str, default="0")
+    parser.add_argument("-d", "--directory", type=str, default="/tmp")
+    parser.add_argument("-s", "--scale", type=int, default=0)
     args = parser.parse_args()
     return args
 
+
 def get_modified_time(path):
     localtime = time.localtime(os.path.getmtime(path))
-    return time.strftime('%Y%m%d%H%M%S', localtime)
+    return time.strftime("%Y%m%d%H%M%S", localtime)
+
 
 def get_args_hash(args):
-    return hashlib.md5(str(args).encode(encoding='UTF-8')).hexdigest()
+    return hashlib.md5(str(args).encode(encoding="UTF-8")).hexdigest()
+
 
 def get_file_name(path):
     name = os.path.splitext(os.path.basename(path))[0]
     return name
+
 
 def get_target_image_name(args, path):
     filename = get_file_name(path)
@@ -32,11 +37,20 @@ def get_target_image_name(args, path):
     hashcode = get_args_hash(args)
     return f"{filename}-{timestamp}-{hashcode[-5:]}"
 
-def convert_ppt_page_to_img(path, page, scale, imgpath=''):
-    command = f"libreoffice --headless --convert-to pdf {path} --outdir /tmp/".split()
-    process = subprocess.Popen(command, stdin = subprocess.PIPE,
-                            universal_newlines=True, text=True,
-                            stdout = subprocess.PIPE)
+
+def convert_ppt_page_to_img(path, page, scale, imgpath=""):
+    command = (
+        "libreoffice --headless --convert-to pdf".split()
+        + [path]
+        + "--outdir /tmp/".split()
+    )
+    process = subprocess.Popen(
+        command,
+        stdin=subprocess.PIPE,
+        universal_newlines=True,
+        text=True,
+        stdout=subprocess.PIPE,
+    )
 
     process.wait()
 
@@ -47,24 +61,35 @@ def convert_ppt_page_to_img(path, page, scale, imgpath=''):
     else:
         return convert_pdf_page_to_img(pdfpath, page, scale, imgpath)
 
-def convert_pdf_page_to_img(path, page, scale, imgpath=''):
+
+def convert_pdf_page_to_img(path, page, scale, imgpath=""):
     if shutil.which("pdftoppm"):
         imgpath_sans = os.path.splitext(image_path)[0]
-        command = f"pdftoppm -png -f {page} -scale-to {scale} -singlefile {path} {imgpath_sans}".split()
-        process = subprocess.Popen(command, stdin = subprocess.PIPE,
-                                   universal_newlines=True, text=True,
-                                   stdout = subprocess.PIPE)
+        command = (
+            f"pdftoppm -png -f {page} -scale-to {scale} -singlefile".split()
+            + [path]
+            + [imgpath_sans]
+        )
+        process = subprocess.Popen(
+            command,
+            stdin=subprocess.PIPE,
+            universal_newlines=True,
+            text=True,
+            stdout=subprocess.PIPE,
+        )
         process.wait()
 
         if process.returncode != 0 or not os.path.exists(imgpath):
             raise Exception(process.returncode)
     else:
-        import fitz 
+        import fitz
+
         page = page - 1
         doc = fitz.open(path)
         pix = doc[page].get_pixmap()  # render page to an image
-        pix.save(imgpath) 
+        pix.save(imgpath)
     return imgpath
+
 
 def get_path_and_page(link, page):
     """
@@ -74,7 +99,7 @@ def get_path_and_page(link, page):
     fullpath = link
     if link[:5] == "file:" or link[:4] == "pdf:":
         # get ~/manual/DL_en.pdf::790++0.00
-        fullpath = link.split(":", 1)[1] 
+        fullpath = link.split(":", 1)[1]
 
     # get (~/manual/DL_en.pdf, 790)
     if "::" in fullpath:
@@ -93,6 +118,6 @@ if __name__ == "__main__":
     path, page = get_path_and_page(link, args.page)
     image_name = get_target_image_name(args, path)
     folder = os.path.expanduser(args.directory)
-    image_path = os.path.join(folder, image_name+".png")
+    image_path = os.path.join(folder, image_name + ".png")
     scale = args.scale
     print(convert_ppt_page_to_img(path, page, scale, image_path), end="")
